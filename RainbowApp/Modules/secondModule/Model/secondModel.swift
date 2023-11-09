addbd435f6b7f801e40b8e5ff2e0ecdd7cced76a
Dmitry Lorents <zmey.task@gmail.com>
2023-11-09 17:22:03 +0300

    Add modules  & move info.plist file

---
+++ RainbowApp/Modules/secondModule/Model/secondModel.swift
@@ -0,0 +1,10 @@
+//
+//  5Model.swift
+//  RainbowApp
+//
+//  Created by Dmitry on 09.11.2023.
+//
+
+import Foundation
+
+
---
+++ RainbowApp/Modules/secondModule/View/secondView.swift
@@ -0,0 +1,30 @@
+//
+//  5View.swift
+//  RainbowApp
+//
+//  Created by Dmitry on 09.11.2023.
+//
+
+import UIKit
+
+class secondView: UIView {
+
+    //MARK: - Parameters
+    
+    
+    
+    //MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Methods
+    
+    
+}
