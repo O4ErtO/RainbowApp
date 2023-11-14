54efdf0103457b26b6636c93115370b302217602
Artem Moiseev <moiseev@physics.msu.ru>
2023-11-14 16:10:02 +0300

    add UserDefaultService

---
+++ RainbowApp/Modules/settingsModule/Model/UserDefaultService.swift
@@ -0,0 +1,32 @@
+//
+//  UserDefaultService.swift
+//
+
+import Foundation
+
+final class UserDefaultService {
+    
+    static let shared = UserDefaultService()
+    private let defaults = UserDefaults.standard
+    
+    // Сохраняем любое Codable
+    func saveData<T: Codable>(type: T, forKey key: String) {
+        if let encoded = try? JSONEncoder().encode(type) {
+            defaults.set(encoded, forKey: key)
+        }
+    }
+    
+    // Считываем любое Codable
+    func getData<T: Codable>(forKey key: String) -> T? {
+        if let saveData = defaults.data(forKey: key) {
+            if let decoded = try? JSONDecoder().decode(T.self, from: saveData) {
+                return decoded
+            }
+        }
+        return nil
+    }
+    
+    func removeData(forKey key: String) {
+        defaults.removeObject(forKey: key)
+    }
+}
--- RainbowApp/Modules/settingsModule/Model/setingsModel.swift
+++ RainbowApp/Modules/settingsModule/Model/setingsModel.swift
@@ -2,7 +2,34 @@
 //  RainbowApp
 //
 
-import Foundation
+import UIKit
+
+struct SettingsModel: Codable {
+    var gameTime: Int
+    var changeTime: Int
+    var isCountTask: Bool
+    var selectedColors: [Bool]
+    var textSize: Int
+    var isTextBackground: Bool
+    var backgroundColor: String
+    var wordPosition: String
+}
+    
+    enum CodingKeys: String, CodingKey {
+        case gameTime, changeTime, isCoutTask, selectedColors,
+             textSize, isTextBackground,backgrondColor, wordPosition
+}
+
+enum BgColor: String {
+    case black = "black"
+    case gray = "gray"
+    case white = "white"
+}
+
+enum WordPosition {
+    case random
+    case center
+}
 
 enum SettingsLabel {
     static let gameTime = "Время игры"
